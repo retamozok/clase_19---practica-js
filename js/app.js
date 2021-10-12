73cb4e3d5c9fc27b0eba14229189b9439a7a07b2
retamozok <matiasagustin.1823@gmail.com>
2021-10-12 11:34:11 -0300

    Clase 12/10

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $("#cambiar").click(function(){
+
+        $("#titulo").text("el titulo ha cambiado")
+    })
+
+    $("#cambiarsub").click(function(){
+        $("#subtitulo").text("el subtitulo ha cambiado")
+    })
+
+
+    $("sumar")
+
+
+});
+
