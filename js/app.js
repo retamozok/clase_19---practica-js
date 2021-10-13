6514d6de3e2ea05d048531d9cb09318b33d1189c
retamozok <matiasagustin.1823@gmail.com>
2021-10-13 11:16:02 -0300

    Clase 13/10 - Calculadora

--- js/app.js
+++ js/app.js
@@ -1,17 +1,56 @@
+console.log("prueba js")
+
+
 $(document).ready(function(){
 
-    $("#cambiar").click(function(){
-
-        $("#titulo").text("el titulo ha cambiado")
-    })
-
-    $("#cambiarsub").click(function(){
-        $("#subtitulo").text("el subtitulo ha cambiado")
-    })
 
 
-    $("sumar")
+    $("#calcular").click(function(){
+
+        let valor1 = $("#valor1").val()
+        let valor2 = $("#valor2").val()
+        let operacion = $("#operacion").val()
+        let resultado = 0
+            valor1 = parseFloat(valor1)
+            valor2 = parseFloat(valor2)
+            
+     if (operacion == "suma"){
+        resultado = valor1 + valor2
+        $("#resultado").text(resultado)
+     }
+    
+     else if (operacion == "resta"){
+        resultado = valor1 - valor2
+        $("#resultado").text(resultado)
+     }
+  
+     else if (operacion == "multi"){
+        resultado = valor1 * valor2
+        $("#resultado").text(resultado)
+        console.log(resultado)
+     } 
+
+     else if (operacion == "div"){
+        resultado = valor1 / valor2
+        $("#resultado").text(resultado)
+        console.log(resultado)
+     } 
 
 
-});
+     else if (operacion =="raiz"){
+       resultado =  Math.sqrt(valor1)
+       $("#resultado").text(`la raiz del primer numero es ${resultado}`)
+     }
 
+     
+     
+     else {
+        $("#resultado").text(`Operacion erronea solo se permita suma,resta,multi,`)  
+     }
+    
+    
+    });
+
+
+
+})
